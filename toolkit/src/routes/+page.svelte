d144d6342b718c64bda537d908c06b4a814fda1c
Karen <navarreteruizkarensofia@gmail.com>
2026-05-11 19:23:25 -0600

    SFSDFSD

--- toolkit/src/routes/+page.svelte
+++ toolkit/src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex min-h-screen items-center justify-center bg-base-200 p-4">
+    <div class="card lg:card-side bg-base-100 shadow-xl max-w-4xl w-full">
+        
+        <div class="card-body justify-center lg:w-1/2 p-10">
+            <h1 class="card-title text-4xl font-extrabold text-slate-800 mb-4">
+                Lógica y esctructuras discretas
+            </h1>
+            <p class="text-slate-600 text-lg leading-relaxed grow-0">
+                En este curso vamos a desarrollar conocimientos y habilidades para abstraer, identificar, construir, y desarrollar modelos matemáticos discretos formales, analizando y evaluando herramientas matemáticas para presentar una solución a un problema.
+                Enseguida encontrarás información general sobre el curso y enlaces a información particular del curso y a lo que aprenderás durante el desarrollo del mismo.
+            </p>
+        </div>
+
+        <div class="p-10 flex flex-col justify-center gap-4 lg:w-1/2 bg-slate-50 rounded-b-2xl lg:rounded-r-2xl lg:rounded-bl-none">
+            
+            <a href="/tablas-de-verdad" class="btn border-none bg-slate-500 hover:bg-slate-600 text-white shadow-md text-lg font-normal h-auto py-3">
+                Tablas de Verdad
+            </a>
+            
+            <a href="/operaciones-conjuntos" class="btn border-none bg-slate-500 hover:bg-slate-600 text-white shadow-md text-lg font-normal h-auto py-3">
+                Operaciones con conjuntos
+            </a>
+            
+            <button class="btn border-none bg-slate-500 hover:bg-slate-600 text-white shadow-md text-lg font-normal h-auto py-3">
+                Parcial 3 
+            </button>
+            
+            <button class="btn border-none bg-slate-500 hover:bg-slate-600 text-white shadow-md text-lg font-normal h-auto py-3">
+                Parcial 4
+            </button>
+            
+        </div>
+    </div>
+</main>
